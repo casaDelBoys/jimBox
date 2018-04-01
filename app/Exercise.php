c3d26dbad7c362d611b1490349943284b9578fa5
TommyCakes <tomhwarhurst@aol.com>
2018-04-01 14:40:29 +0100

    Updated model relationships

--- app/Exercise.php
+++ app/Exercise.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Exercise extends Model
 {
     protected $fillable = ['name', 'description', 'image_path'];
+
+    public function routines()
+    {
+        return $this->belongsToMany('App\Routines');
+    }
 }
